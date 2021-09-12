29a7dd50f25bdf77ef9b5e98a079385b69f1ccab
xy1-pixel <76541196+xy1-pixel@users.noreply.github.com>
2021-09-12 16:16:23 +1000

    design on button
    create two styles of button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import {Button} from './components/Button'
+import "./components/button.css";
+
+function btnlogin(){
+  console.log("Login");
+}
+
+function btnregister(){
+  console.log("register");
+}
 
 function App() {
-  return <div className='App'>Hello</div>
+  return <div className='App'>
+    <Button className="tiffbutton" onClick={btnlogin} >Login to USN</Button>
+    <Button className="bluebutton" onClick={btnregister} >Register</Button>
+  </div>
 }
 
 export default App
---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./button.css";
+
+
+export const Button =({
+    children,
+    onClick,
+    className
+}) => {
+ return(
+        <button className={className} onClick={onClick}>
+            {children}
+        </button>
+    )
+};
+
+
+
+
