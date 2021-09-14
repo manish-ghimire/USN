635d818f561547b0ac4b2f68c3c0352bb56042ab
Runqing-Programer <61403704+Runqing-Programer@users.noreply.github.com>
2021-09-14 21:25:01 +1000

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import './App.css'
-import Button from './components/Button'
-import FillBoxHead from './components/FillBoxHead/FillBoxHead'
-import { useState } from 'react'
+import TopAppBar from './components/topAppBar/TopAppBar'
+
+
 
 
 function App() {
-  
-  const [username, setUserName] = useState('')
-  const [password, setPassWord] = useState('')
-
-  const login = ()=> {
-    //console.log(username);
-    //console.log(password);
-    //pass to nathan
-  }
 
   return (
-    <div className='App'>
-      <Button onClick={login}>Login</Button>
-      <FillBoxHead textHead='Name' userEntry={setUserName}></FillBoxHead><br/>
-      <FillBoxHead type ='password' textHead="Name" userEntry={setPassWord}></FillBoxHead>
+    <div>
+      <TopAppBar/>
     </div>
   )
 
---
+++ frontend/src/components/topAppBar/TopAppBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import USNLogo from '../../images/Logo.png'
+
+const topAppBar = () => {
+  return (
+    <div>
+      <img src={USNLogo} alt='USN Logo' width='100px'/>
+    </div>
+  )
+}
+
+export default topAppBar
