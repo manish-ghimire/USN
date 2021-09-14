95c7a5de9305eb4d0763508f63b8340205deff42
Runqing-Programer <61403704+Runqing-Programer@users.noreply.github.com>
2021-09-14 20:01:15 +1000

    sep/09

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import './App.css'
 import Button from './components/Button'
+import FillBoxHead from './components/FillBoxHead/FillBoxHead'
+import { useState } from 'react'
+
 
 function App() {
-  const funcMeaw = () => {
-    console.log('console logging')
+  
+  const [username, setUserName] = useState('')
+  const [password, setPassWord] = useState('')
+
+  const login = ()=> {
+    //console.log(username);
+    //console.log(password);
+    //pass to nathan
   }
 
   return (
     <div className='App'>
-      <Button onClick={funcMeaw}>Hi Manish</Button>
+      <Button onClick={login}>Login</Button>
+      <FillBoxHead textHead='Name' userEntry={setUserName}></FillBoxHead><br/>
+      <FillBoxHead type ='password' textHead="Name" userEntry={setPassWord}></FillBoxHead>
     </div>
   )
+
 }
 
 export default App
