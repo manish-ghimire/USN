281cbb2b275d58302ae914d12372235fc681e45f
Runqing-Programer <61403704+Runqing-Programer@users.noreply.github.com>
2021-09-15 14:14:14 +1000

    delet

--- frontend/src/components/topAppBar/TopAppBar.jsx
+++ frontend/src/components/topAppBar/TopAppBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import WhatEver from '../../images/Logo.png'
+import Logo from '../../images/Logo.png'
 
 const TopAppBar = () => {
   return (
     <div>
       Hello USN Team
-      <img src={WhatEver} alt='Logo' width='100px' />
+      <img src={Logo} alt='Logo' width='100px' />
     </div>
   )
 }
