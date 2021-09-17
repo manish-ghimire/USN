b452ebcd7aa553e4a9f63b99e6e5795994ad1639
manish-ghimire <manishghimire@hotmail.com>
2021-09-17 18:08:11 +1000

    Login basic layout

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Image from '../../images/Login_page_image.jpg'
+import Grid from '@material-ui/core/Grid'
+import { Box } from '@material-ui/core'
 
 const Login = () => {
-  return <div>This is login page</div>
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <Box overflow='hidden' height='100vh'>
+          <img src={Image} alt='USN Logo' />
+        </Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box bgcolor='gray' height='100vh'>
+          def
+        </Box>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Login
