c9ccfc1af75107ceac7ab746ac082727abe456be
manish-ghimire <manishghimire@hotmail.com>
2021-09-17 23:10:21 +1000

    Navbar design completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,29 @@ import Register from './pages/Register/Register'
 import Login from './pages/Login/Login'
 import Error from './pages/Error/Error'
 import User from './pages/User/User'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#A4E5E0',
+    },
+  },
+})
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Switch>
-          <Route path='/register' component={Register} />
-          <Route exact path='/' component={Login} />
-          <Route path='/user/:id' component={User} />
-          <Route path='/error' component={Error} />
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path='/register' component={Register} />
+            <Route exact path='/' component={Login} />
+            <Route path='/user/:id' component={User} />
+            <Route path='/error' component={Error} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   )
 }
