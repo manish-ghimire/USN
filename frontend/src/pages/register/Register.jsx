c6aa7ef8014a80cbf70de811720775b269cd236c
manish-ghimire <manishghimire@hotmail.com>
2021-09-20 22:09:42 +1000

    Register page

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -4,29 +4,19 @@ import Logo from '../../images/Logo.png'
 import RegisterForm from './RegisterForm'
 
 const Register = () => {
-<<<<<<< HEAD
-
   return (
-    <div className="register-container">
+    <div className='register-container'>
       <div className='leftDiv'>
-        <img src={Logo} alt="USN logo" className='center' width='100px'/>
+        <img src={Logo} alt='USN logo' className='center' width='100px' />
       </div>
       <div className='rightDiv'>
-        <div className='slogen'>
-          Join USNow!
+        <div className='slogan-form-box'>
+          <div className='slogen'>Join USNow!</div>
+          <RegisterForm></RegisterForm>
         </div>
-        <div>
-         <RegisterForm></RegisterForm>
-        </div>
-
       </div>
-
     </div>
   )
-
-=======
-  
->>>>>>> e3c4d810266dc19610e8cdaa2dce1328ac215216
 }
 
 export default Register
