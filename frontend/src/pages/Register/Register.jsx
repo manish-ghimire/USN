ccea843c8cb604f24f832f51e2cfeebc4cc83b84
Runqing-Programer <61403704+Runqing-Programer@users.noreply.github.com>
2021-09-20 20:54:10 +1000

    merge

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import './Register.scss'
+import Logo from '../../images/Logo.png'
+import RegisterForm from './RegisterForm'
 
 const Register = () => {
-  return <div>This is register page</div>
+
+  
+  return (
+    <div className="register-container">
+      <div className='leftDiv'>
+        <img src={Logo} alt="USN logo" className='center' width='100px'/>
+      </div>
+      <div className='rightDiv'>
+        <div className='slogen'>
+          Join USNow!
+        </div>
+        <div>
+         <RegisterForm></RegisterForm>
+        </div>
+
+      </div>
+
+    </div>
+  )
+
 }
 
 export default Register
