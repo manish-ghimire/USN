4bd61e0ecd68d5443e5c425f58f15e2c500434e5
54x1 <five4x1@gmail.com>
2021-09-28 02:27:03 +1000

    fixed admin sorta

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,77 +6,78 @@ const verify = require("./verify");
 //Register
 // https://reqbin.com/
 // post--> http://localhost:5000/api/auth/register
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   try {
-    if (!req.body.username || !req.body.email || !req.body.password) {
-      console.log({ errors: 'All fields are required' })
-      return res.status(422).json({ error: 'All fields are required' })
-    } else {
-      // find email or username
-      User.findOne({
-        $or: [
-          {
-            email: req.body.email,
-          },
-          {
-            username: req.body.username,
-          },
-        ],
-      })
-        .then((user) => {
-          if (user) {
-            // backend error stuff
-            let errors = {}
-            if (user.username === req.body.username) {
-              errors.username = 'Username already exists'
-            }
-            if (user.email === req.body.email) {
-              errors.email = 'Email already exists'
-            }
-            console.log({ errors: errors })
-            return res.status(500).json(user)
-          } else {
-            // fetch user from body
-            const newUser = new User({
-              username: req.body.username,
-              email: req.body.email,
-              password: req.body.password,
-            })
-            // hash password
-            bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err
-                newUser.password = hash
-                // save newU user
-                newUser
-                  .save()
-                  .then((user) => res.status(200).json(user))
-                  .catch((err) => console.log(err))
-              })
-            })
-          }
+    if(!req.body.username || !req.body.email || !req.body.password){
+          console.log({errors: "All fields are required"});
+      return res.status(422).json({error:"All fields are required"});
+    } else{
+    // find email or username
+    User.findOne({
+            $or: [{
+                email: req.body.email
+            }, {
+                username: req.body.username
+            }]
+        }).then(user => {
+            if (user) {
+              // backend error stuff
+                let errors = {};
+                if (user.username === req.body.username) {
+                    errors.username = "Username already exists";
+                }
+                if (user.email === req.body.email) {
+                    errors.email = "Email already exists";
+                }
+                console.log({errors:errors});
+                return res.status(500).json(user);
+
+            } else {
+              // fetch user from body
+              const newUser = new User({
+                             username: req.body.username,
+                             email: req.body.email,
+                             password: req.body.password
+                         });
+                         // hash password
+                         bcrypt.genSalt(10, (err, salt) => {
+                             bcrypt.hash(newUser.password, salt, (err, hash) => {
+                                 if (err) throw err;
+                                 newUser.password = hash;
+                                 // save newU user
+                                 newUser.save().then(
+                                   (user) => res.status(200).json(user)
+                                 ).catch((err) => console.log(err));
+                             });
+                         });
+                     }
         })
         .catch((err) => {
-          return res.status(500).json({
-            error: err,
-          })
-        })
-    }
+            return res.status(500).json({
+                error: err
+            });
+        });
+      }
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-})
+});
+
+
+
+
+
 
 //Login
 // https://reqbin.com/
 // post--> http://localhost:5000/api/auth/login
 // store refresh token in mongodb
-let refreshTokens = []
-router.post('/login', (req, res) => {
+let refreshTokens = [];
+router.post("/login", (req, res) => {
   try {
     // check if username or email
     if (req.body.username.indexOf('@') > -1) {
-      console.log('@here')
+      console.log("@here");
       // email login stuff
      User.findOne({ email: req.body.username }).then((validUser) => {
         if(!validUser){
@@ -97,9 +98,14 @@ router.post('/login', (req, res) => {
               console.log("Invalid Credentials");
               res.status(500).json("Invalid Credentials");
           }
+        });
+      }
+        catch (err){
+          res.status(500).json(err);
         }
-      })
-    } else {
+      }
+      });
+    }else{
       // username login stuff
             console.log("no@");
     User.findOne({ username: req.body.username }).then((validUser) => {
@@ -121,15 +127,19 @@ router.post('/login', (req, res) => {
               console.log("Invalid Credentials");
               res.status(500).json("Invalid Credentials");
           }
-        }
-      })
+        });
+    }
+      catch (err){
+        res.status(500).json(err);
+      }
+    }
+      });
     }
   } catch (err) {
     res.status(500).json(err)
   }
-})
+});
 
-<<<<<<< HEAD
 // const verify = (req, res, next) => {
 //   const authHeader = req.headers.authorization;
 //   if (authHeader){
@@ -158,39 +168,40 @@ router.post('/login', (req, res) => {
 // }
 // });
 
+
 // refresh token
 // https://reqbin.com/
 // http://localhost:5000/api/auth/refresh/
 
-router.post('/refresh', (req, res) => {
-  const refreshToken = req.body.token
+router.post("/refresh", (req, res) => {
+  const refreshToken = req.body.token;
 
   if (!refreshToken) {
-    return res.status(401).json('Not authenticated!')
+    return res.status(401).json("Not authenticated!")
   }
-  if (!refreshTokens.includes(refreshToken)) {
-    return res.status(403).json('Not valid token!')
+  if(!refreshTokens.includes(refreshToken)){
+    return res.status(403).json("Not valid token!")
   }
-  jwt.verify(refreshToken, 'myRefreshSecretKey', (err, user) => {
-    console.log(err)
-    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
-    
+  jwt.verify(refreshToken, "myRefreshSecretKey", (err, user) => {
+    console.log(err);
+    refreshTokens  = refreshTokens.filter((token) => token !== refreshToken);
+
     const newAccessToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "mySecretKey", { expiresIn: "15m" });
     const newRefreshToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "myRefreshSecretKey", { expiresIn: "15m" });
 
-    refreshTokens.push(newRefreshToken)
+    refreshTokens.push(newRefreshToken);
 
     res.status(200).json({
       accessToken: newAccessToken,
       refreshToken: newRefreshToken,
-    })
-  })
-})
+    });
+});
+});
 
-router.post('/logout', verify, (req, res) => {
-  const refreshToken = req.body.token
-  refreshTokens = refreshTokens.filter((token) => token !== refreshToken)
-  res.status(200).json('Logged Out Success!')
-})
+router.post("/logout", verify, (req, res) => {
+const refreshToken = req.body.token;
+refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+res.status(200).json("Logged Out Success!");
+});
 
-module.exports = router
+module.exports = router;
