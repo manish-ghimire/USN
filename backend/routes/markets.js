66df3d9c0c16a5b055fb901a853107ab681ae546
manish-ghimire <manishghimire@hotmail.com>
2021-10-08 23:01:08 +1100

    bla bla

--- backend/routes/markets.js
+++ backend/routes/markets.js
@@ -1,31 +1,32 @@
-const router = require("express").Router();
-const Market = require("../models/Market");
-const User = require("../models/User");
-const verify = require("./verify");
-const Comment = require("../models/Comment");
+const router = require('express').Router()
+const Market = require('../models/Market')
+const User = require('../models/User')
+const verify = require('./verify')
+const Comment = require('../models/Comment')
 
-// http://localhost:5000/api/item/:itemId/comments
-router.post("/item/:itemId/comments", verify, async (req, res) => {
+// http://localhost:5000/api/market/
+
+router.post('/item/:itemId/comments', verify, async (req, res) => {
   const newCom = new Comment({
     userId: req.user.id,
     desc: req.body.desc,
-    commentToId: req.params.itemId
-  });
+    commentToId: req.params.itemId,
+  })
 
   // try {
-    if (!newCom){
-    res.status(422).json({error: "Post is Empty"});
-  }
-  else{
-    const savedCom = await newCom.save();
-    res.status(200).json(savedCom);
+  if (!newCom) {
+    res.status(422).json({ error: 'Post is Empty' })
+  } else {
+    const savedCom = await newCom.save()
+    res.status(200).json(savedCom)
   }
   // }
   // catch (err) {
   //   res.status(500).json(err);
   // }
-});
-router.post("/item/", verify, async (req, res) => {
+})
+
+router.post('/item/', verify, async (req, res) => {
   const newMark = new Market({
     userId: req.user.id,
     itemName: req.body.itemName,
@@ -34,168 +35,118 @@ router.post("/item/", verify, async (req, res) => {
     itemLocation: req.body.itemLocation,
     img: req.body.img,
     role: req.body.role,
-    postToId: req.body.postToId
-  });
+    postToId: req.body.postToId,
+  })
   // try {
-    if (!newMark){
-    res.status(422).json({error: "All fields are required"});
-  }
-  else{
-    const saveMark = await newMark.save();
-    res.status(200).json(saveMark);
+  if (!newMark) {
+    res.status(422).json({ error: 'All fields are required' })
+  } else {
+    const saveMark = await newMark.save()
+    res.status(200).json(saveMark)
   }
   // }
   // catch (err) {
   //   res.status(500).json(err);
   // }
-});
-//update a post
-// http://localhost:5000/api/market:id
-router.put("/item/:itemId", verify, async (req, res) => {
+})
+
+router.put('/item/:itemId', verify, async (req, res) => {
   try {
-    const post = await Market.findById(req.params.itemId);
+    const post = await Market.findById(req.params.itemId)
 
     if (post.userId === req.user.id || req.user.isAdmin) {
-      try{
+      try {
         const updatedPost = await Market.findByIdAndUpdate(
           req.params.itemId,
           {
             $set: req.body,
           },
           { new: true }
-        );
-         res.status(200).json(updatedPost);
+        )
+        res.status(200).json(updatedPost)
+      } catch (err) {
+        res.status(500).json(err)
       }
-      catch (err){
-        res.status(500).json(err);
-      }
-  }
-  else {
-      res.status(403).json("You can update only your post!");
-  }
-
+    } else {
+      res.status(403).json('You can update only your post!')
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-//delete a post
+})
 
-router.delete("/item/:itemId", verify, async (req, res) => {
+router.delete('/item/:itemId', verify, async (req, res) => {
   try {
-    const post = await Market.findById(req.params.itemId);
+    const post = await Market.findById(req.params.itemId)
     if (post.userId === req.user.id || req.user.isAdmin) {
-        post.delete();
-      res.status(200).json("The post has been deleted!");
+      post.delete()
+      res.status(200).json('The post has been deleted!')
     } else {
-      res.status(401).json("You can delete only your post!");
+      res.status(401).json('You can delete only your post!')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-//like / unlike a post
+})
 
-router.put("/item/:itemId/like", verify, async (req, res) => {
+router.put('/item/:itemId/like', verify, async (req, res) => {
   try {
-    const post = await Market.findById(req.params.itemId);
+    const post = await Market.findById(req.params.itemId)
     if (!post.likes.includes(req.user.id)) {
-      await post.updateOne({ $push: { likes: req.user.id } });
-      res.status(200).json("The post has been liked");
+      await post.updateOne({ $push: { likes: req.user.id } })
+      res.status(200).json('The post has been liked')
     } else {
-      await post.updateOne({ $pull: { likes: req.user.id } });
-      res.status(200).json("The post has been unliked");
+      await post.updateOne({ $pull: { likes: req.user.id } })
+      res.status(200).json('The post has been unliked')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-// router.get("/", verify, async (req, res) => {
-//   posts = await Market.find();
-//   res.status(200).json(posts);
-// });
-//Get a post
-router.get("/item/:itemId", verify, async (req, res) => {
+})
+
+router.get('/item/:itemId', verify, async (req, res) => {
   try {
-    const post = await Market.findById(req.params.itemId);
-    if (post){
-    res.status(200).json(post);
-  }
-  else {
-      res.status(500).json("can't find item!");
-  }
+    const post = await Market.findById(req.params.itemId)
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(500).json("can't find item!")
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-// get filtered post
-// http://localhost:5000/api/market/item?user=:id&?=role=:role
-router.get("/item", verify, async (req, res) => {
-const role = req.query.role;
-const userPosts = req.query.user;
-// try{
-console.log(userPosts);
-let posts;
-if (userPosts){
-  posts = await Market.find({userId:userPosts});
-  res.status(200).json(posts);
-}else if (role){
-    posts = await Market.find({role: {
-      $in: [role],
-    },
-  });
-    res.status(200).json(posts);
-}
-else{
-  res.status(500).json("cant find item");
-}
-// }
-// catch (err){
-//     res.status(500).json(err);
-// }
-});
-//Get All Post
-// http://localhost:5000/api/market/
-router.get("/", verify, async (req, res) => {
+})
 
-  const items = await Market.find();
-  res.status(200).json(items);
-});
+router.get('/item', verify, async (req, res) => {
+  const role = req.query.role
+  const userPosts = req.query.user
+  console.log(userPosts)
+  let posts
+  if (userPosts) {
+    posts = await Market.find({ userId: userPosts })
+    res.status(200).json(posts)
+  } else if (role) {
+    posts = await Market.find({
+      role: {
+        $in: [role],
+      },
+    })
+    res.status(200).json(posts)
+  } else {
+    res.status(500).json('cant find item')
+  }
+})
 
-// find comments
-// http://localhost:5000/api/post/:itemId/comments
-router.get("/item/:itemId/comments", verify, async (req, res) => {
-  const com = await Comment.find({commentToId: req.params.itemId});
-  console.log(com);
-  res.status(200).json(com);
-});
+router.get('/', verify, async (req, res) => {
+  const items = await Market.find()
+  res.status(200).json(items)
+})
 
-// //get timeline posts
-//
-// router.get("/timeline/:userId", async (req, res) => {
-//   try {
-//     const currentUser = await User.findById(req.params.userId);
-//     const userPosts = await Post.find({ userId: currentUser._id });
-//     const followerPosts = await Promise.all(
-//       currentUser.followings.map((followerId) => {
-//         return Post.find({ userId: followerId });
-//       })
-//     );
-//     res.status(200).json(userPosts.concat(...followerPosts));
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-//
-// //get all user's posts
-//
-// router.get("/profile/:username", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ username: req.params.username });
-//     const posts = await Post.find({ userId: user._id });
-//     res.status(200).json(posts);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// extra
+router.get('/item/:itemId/comments', verify, async (req, res) => {
+  const com = await Comment.find({ commentToId: req.params.itemId })
+  console.log(com)
+  res.status(200).json(com)
+})
 
-module.exports = router;
+module.exports = router
--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useLayoutEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import axios from 'axios'
 import Box from '@mui/material/Box'
@@ -12,159 +12,95 @@ import Navbar from '../../components/Navbar/Navbar'
 import Post from '../../components/Post/Post.jsx'
 import Card from '../../components/Card/Card'
 import { Avatar, Container, Grid, Hidden } from '@mui/material'
+import { SettingsInputCompositeSharp } from '@mui/icons-material'
 
 const UserProfile = ({ setCircle }) => {
   const history = useHistory()
   const { userId } = useParams()
   const [user, setUser] = useState({})
   const [unis, setUnis] = useState([])
-  // const [posts, setPosts] = useState([])
+  const [blas, setBla] = useState([
+    { uniName: 'University of Canberra', classOf: '2019' },
+    { uniName: 'University of Sydney', classOf: '2021' },
+  ])
+  const [clubs, setClubs] = useState([])
   const accessToken = localStorage.getItem('accessToken')
 
   useEffect(() => {
-    if (accessToken) {
-      const fetchData = async () => {
-        try {
-          const successUser = await axios.get(`/user/${userId}`, {
+    console.log('FETCH DATA START')
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const userUrl = `/user/${userId}`
+      const successUser = await axios.get(userUrl, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+
+      const uniLists = []
+
+      for (var i = 0; i < successUser.data.study.length; i++) {
+        console.log('index ' + i + ' Step 1')
+        const successUni = await axios.get(
+          `/uni/${successUser.data.study[i].uniId}`,
+          {
             headers: {
               Authorization: `Bearer ${accessToken}`,
             },
-          })
+          }
+        )
+        console.log('index ' + i + ' Step 2 ' + successUni)
+        uniLists.push({
+          uniName: successUni.data.uniName,
+          classOf: successUser.data.study[i].classOf,
+        })
 
-          const uniLists = []
-          successUser.data.study.map(async (uni, index) => {
-            const successUni = await axios.get(`/uni/${uni.uniId}`, {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-            })
-            uniLists.push({
-              uniName: successUni.data.uniName,
-              classOf: successUser.data.study[index].classOf,
-            })
-          })
-          setUnis(uniLists)
-          setUser(successUser.data)
-        } catch (error) {
-          console.log('Error fetching data', error)
-        }
+        console.log('index ' + i + ' Step 3')
       }
-      fetchData()
-    } else {
-      console.log('Im here')
-      // history.push('/login', { text: 'hellooooooo' })
-    }
-  }, [accessToken, userId])
+      setUnis(uniLists)
 
-  const posts = [
-    {
-      _id: 1,
-      text: 'This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. ',
-      shares: 10,
-      likes: 52,
-    },
-    {
-      _id: 2,
-      text: 'This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. This is something related to user. ',
-      shares: 10,
-      likes: 52,
-    },
-  ]
+      /*
+      await successUser.data.study.map(async (uni, index) => {
+        console.log('STEP3')
+        const successUni = await axios.get(`/uni/${uni.uniId}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        console.log('STEP3.5' + successUni)
+        uniLists.push({
+          uniName: successUni.data.uniName,
+          classOf: successUser.data.study[index].classOf,
+        })
+        console.log('STEP4 push complete')
+        setUnis(uniLists)
+        console.log('STEP5 setunis')
+      })
+      */
+    } catch (error) {
+      console.log('Error fetching data', error)
+    }
+  }
+
+  function tester() {}
+
+  console.log('BLAS', blas)
+  console.log('UNIS', unis)
 
   return (
     <>
-      <Navbar />
-      <Container disableGutters maxWidth='xl' className='container'>
-        <Grid container>
-          <Hidden mdDown>
-            <Grid item md={3}>
-              <Card>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    paddingBottom: '20px',
-                  }}
-                >
-                  <Avatar
-                    alt='Memy Sharp'
-                    src='https://picsum.photos/400/400'
-                    sx={{ width: 100, height: 100, margin: '25px 0 15px 0' }}
-                  />
-                  <div>
-                    {user.fName} {user.lName}kkkkkkkk
-                  </div>
-                  <div>{user.username}</div>
-                  <div>{user.isFrom}</div>
-                  <div>{user.role}</div>
-                </Box>
-                <Divider />
-                <List>
-                  {['A'].map((uni, index) => (
-                    <h1 key={index}>{console.log(unis[0].classOf)}</h1>
-                  ))}
-                  <ListItem button>
-                    <ListItemIcon>
-                      <SchoolIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={'uni.uniNameeeeeee'}
-                      secondary={'uni.classOf'}
-                      onClick={() => console.log('list item clicked')}
-                    />
-                  </ListItem>
-                </List>
-                <Divider />
-                <List>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <SchoolIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={'Engineering Society Club'}
-                      secondary={'University of Canberra'}
-                      onClick={() => console.log('list item clicked')}
-                    />
-                  </ListItem>
-                </List>
-              </Card>
-            </Grid>
-          </Hidden>
-          <Hidden mdUp>
-            <Grid item xs={12}>
-              <Card height='190px'>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Avatar
-                    alt='Memy Sharp'
-                    src='https://picsum.photos/400/400'
-                    sx={{ width: 100, height: 100, margin: '25px 0 15px 0' }}
-                  />
-                  <div>
-                    {user.fName} {user.lName}zzzzzzzzzzzzzz
-                  </div>
-                </Box>
-              </Card>
-            </Grid>
-          </Hidden>
-          <Grid item xs={9}>
-            <Hidden mdDown>
-              <Post posts={posts} />
-            </Hidden>
-          </Grid>
-          <Hidden mdUp>
-            <Post posts={posts} />
-          </Hidden>
-        </Grid>
-      </Container>
+      {blas.map((bla) => (
+        <h1>Hello</h1>
+      ))}
+      {unis.map((uni) => (
+        <>
+          <h1>{uni.uniName}</h1>
+          <h1>{uni.classOf}</h1>
+        </>
+      ))}
     </>
   )
 }
