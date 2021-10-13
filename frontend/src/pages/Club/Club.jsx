1f23559a245acdd0deba58be4eb9ab059edd6f59
manish-ghimire <manishghimire@hotmail.com>
2021-10-13 13:25:58 +1100

    bla bla

--- frontend/src/pages/Club/Club.jsx
+++ frontend/src/pages/Club/Club.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useParams } from 'react-router'
+import React, { useState, useEffect, useRef } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import axios from 'axios'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
@@ -9,136 +9,158 @@ import ListItem from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import SchoolIcon from '@mui/icons-material/School'
+import Typography from '@mui/material/Typography'
 import Navbar from '../../components/Navbar/Navbar'
 import PostingBox from '../../components/PostingBox/PostingBox.jsx'
 import Post from '../../components/Post/Post.jsx'
 import Card from '../../components/Card/Card'
-import { Avatar, Container, Grid, Hidden } from '@mui/material'
+import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp'
+import ArrowForwardSharpIcon from '@mui/icons-material/ArrowForwardSharp'
+import GroupsIcon from '@mui/icons-material/Groups'
+import EditIcon from '@mui/icons-material/Edit'
+import ControlPointIcon from '@mui/icons-material/ControlPoint'
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary'
+import ArrowRightIcon from '@mui/icons-material/ArrowRight'
+import {
+  Autocomplete,
+  Avatar,
+  Button,
+  Chip,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Hidden,
+  TextField,
+} from '@mui/material'
 
-const drawerWidth = 200
+const drawerWidth = 350
 
 const Club = ({ setCircle }) => {
-  const [mobileOpen, setMobileOpen] = useState(false)
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen)
   }
 
   const history = useHistory()
-  const { clubID } = useParams()
-  const [user, setUser] = useState({})
-  const [study, setStudy] = useState([])
   const [club, setClub] = useState([])
+  const [uni, setUni] = useState([])
+  const [roles, setRoles] = useState([])
+  const [selectedRoles, setSelectedRoles] = useState([])
+  const { clubId } = useParams()
   const [posts, setPosts] = useState([])
+  const [courses, setCourses] = useState([])
+  const [totalCourses, setTotalCourses] = useState([])
+  const [selectedCourses, setSelectedCourses] = useState([])
+  const [faculties, setFaculties] = useState([])
   const accessToken = localStorage.getItem('accessToken')
-
+  const currentUser = JSON.parse(localStorage.getItem('currentUser')).user
+  const currentUni = JSON.parse(localStorage.getItem('currentUni'))
   useEffect(() => {
-    // setCircle(true)
-    // if (accessToken) {
-    //   const fetchData = async () => {
-    //     try {
-    //       if (clubID) {
-    //         const successStudy = await axios.get(`/club/${clubID}`, {
-    //           headers: {
-    //             Authorization: `Bearer ${accessToken}`,
-    //           },
-    //         })
-    //         console.log('here')
-    //         setStudy(successStudy)
-    //         console.log('Success Club', successStudy.data)
-    //       }
-    //       const successClub = await axios.get(`/club/`, {
-    //         headers: {
-    //           Authorization: `Bearer ${accessToken}`,
-    //         },
-    //       })
-    //       console.log('here')
-    //       setClub(successClub)
-    //       console.log('Success Club', successClub.data)
-    //     } catch (error) {
-    //       console.log('Error fetching data', error)
-    //     }
-    //   }
-    //   fetchData()
-    //   console.log(study)
-    // } else {
-    //   console.log('Im here')
-    //   history.push('/login', { text: 'hellooooooo' })
-    // }
-    // setCircle(false)
-  }, [history, setCircle, setClub, accessToken])
+    setCircle(true)
+    if (accessToken) {
+      const fetchData = async () => {
+        try {
+          const successClub = await axios.get(`/club/${clubId}`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+          setClub(successClub.data)
+          console.log('Success club', successClub.data)
+
+          const successUni = await axios.get(`/uni/${successClub.data.uniId}`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+          setUni(successUni.data)
+        } catch (error) {
+          console.log('Error fetching data', error)
+        }
+      }
+      fetchData()
+    } else {
+      console.log('Im here')
+      history.push('/login', { text: 'hellooooooo' })
+    }
+    setCircle(false)
+  }, [history, setCircle, setUni, accessToken])
+
+  console.log('CLUB', club)
 
   const drawer = (
-    <div>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar
-          alt='Memy Sharp'
-          src='https://picsum.photos/400/400'
-          sx={{ width: 100, height: 100, margin: '25px 0 15px 0' }}
-        />
-        <div>
-          {user.fName} {user.lName}
-        </div>
-        <div>{user.username}</div>
-        <div>{user.isFrom}</div>
-        <div>{user.role}</div>
-      </Box>
-      <br />
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <SchoolIcon />
-          </ListItemIcon>
-          <ListItemText
-            primary={'University of Canberra'}
-            secondary={'Expected completion 2021'}
-            onClick={() => console.log('list item clicked')}
-          />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <SchoolIcon />
-          </ListItemIcon>
-          <ListItemText
-            primary={'Frakfurt University'}
-            secondary={'2015-2021'}
-            onClick={() => console.log('list item clicked')}
-          />
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <SchoolIcon />
-          </ListItemIcon>
-          <ListItemText
-            primary={'Engineering Society Club'}
-            secondary={'University of Canberra'}
-            onClick={() => console.log('list item clicked')}
-          />
-        </ListItem>
-      </List>
-    </div>
-  )
-
-  // NO POSTS RELATED
-  let ifNoPosts
-  if (posts.length < 1) {
-    ifNoPosts = (
+    <>
       <Card>
-        <Box sx={{ textAlign: 'center' }}>No Posts to display.</Box>
+        <Box sx={{ textAlign: 'right', color: 'gray' }}>
+          <EditIcon
+            className='editIcon'
+            fontSize='small'
+            onClick={() => console.log('edit clicked')}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            alt='Memy Sharp'
+            src='https://picsum.photos/400/400'
+            sx={{ width: 100, height: 100, margin: '25px 0 15px 0' }}
+          />
+          <h3>{club.clubName}</h3>
+        </Box>
       </Card>
-    )
-  }
+      <Card>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <LocalLibraryIcon />
+            </ListItemIcon>
+            <ListItemText primary={'About us'} />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemIcon>
+              <ArrowRightIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={club.desc}
+              // onClick={() => alert('Go to faculty page')}
+            />
+          </ListItem>
+        </List>
+      </Card>
+      <Card>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <LocalLibraryIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Facts'} />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemIcon>
+              <ArrowRightIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={`${
+                club.clubMembers ? club.clubMembers.length : 'No'
+              } Club members`}
+            />
+          </ListItem>
+        </List>
+      </Card>
+    </>
+  )
 
   const sidebarSkeleton = (
     <>
@@ -205,14 +227,18 @@ const Club = ({ setCircle }) => {
           </Hidden>
           <Grid item xs={8}>
             <Hidden mdDown>
-              {ifNoPosts}
-              <Post posts={posts} />
+              <PostingBox roles={roles} />
+              {posts.map((p, index) => (
+                <Post key={index} posts={p} />
+              ))}
             </Hidden>
           </Grid>
           <Grid item xs={12}>
             <Hidden mdUp>
-              {ifNoPosts}
-              <Post posts={posts} />
+              <PostingBox roles={roles} />
+              {posts.map((p, index) => (
+                <Post key={index} posts={p} />
+              ))}
             </Hidden>
           </Grid>
         </Grid>
